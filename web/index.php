91d9e3fffa9150631771b4db9a4ac8f02be96e64
codepoet80 <codepoet80@github.com>
2026-04-27 17:04:20 -0400

    add meta refresh

--- web/index.php
+++ web/index.php
@@ -48,6 +48,7 @@ $regular_count = count($news_regular);
 <html lang="en">
 <head>
 <meta charset="utf-8">
+<meta http-equiv="refresh" content="3600">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Daily Briefing &mdash; <?php echo h($today_label); ?></title>
 <link rel="stylesheet" type="text/css" href="style.css">
